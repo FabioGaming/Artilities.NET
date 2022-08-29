8f312e163c3678a77a43478b11f7359255b9f1d1
Slimakoi <slimeytoficial@gmail.com>
2022-08-29 11:45:56 +0100

    Added objects

--- Artilities.NET/Artilities.cs
+++ Artilities.NET/Artilities.cs
@@ -1,11 +1,110 @@
 ﻿using Newtonsoft.Json.Linq;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
 using System.Net;
+using static Artilities.users;
 
 namespace Artilities
 {
+    public class Responses
+    {
+        public class Idea
+        {
+            public Idea(JObject json)
+            {
+                english = (string?)json["generated_idea"]["eng"];
+                russian = (string?)json["generated_idea"]["ru"];
+                statusCode = (int?)json["status_code"];
+                delayTime = (int?)json["execution_time"];
+                raw = json;
+            }
+
+            public string? english { get; set; }
+            public string? russian { get; set; }
+            public int? statusCode { get; set; }
+            public int? delayTime { get; set; }
+            public JObject raw { get; set; }
+        }
+
+        public class Challenge
+        {
+            public Challenge(JObject json)
+            {
+                english = (string?)json["generated_challenge"]["eng"];
+                russian = (string?)json["generated_challenge"]["ru"];
+                statusCode = (int?)json["status_code"];
+                delayTime = (int?)json["execution_time"];
+                raw = json;
+            }
+
+            public string? english { get; set; }
+            public string? russian { get; set; }
+            public int? statusCode { get; set; }
+            public int? delayTime { get; set; }
+            public JObject raw { get; set; }
+        }
+
+
+        public class DictionaryEntry
+        {
+            public DictionaryEntry(JObject json)
+            {
+                word = (string?)json["query_results"][0][0];
+                description = (string?)json["query_results"][0][1];
+                statusCode = (int?)json["status_code"];
+                delayTime = (int?)json["execution_time"];
+                raw = json;
+            }
+
+            public string? word { get; set; }
+            public string? description { get; set; }
+            public int? statusCode { get; set; }
+            public int? delayTime { get; set; }
+            public JObject raw { get; set; }
+        }
+
+        public class Banner
+        {
+            public Banner(JObject json)
+            {
+                bannerUrl = (string?)json["details"]["banner_url"];
+                bannerImage = (string?)json["details"]["bannerImage"];
+                language = (string?)json["details"]["language"];
+                statusCode = (int?)json["status_code"];
+                delayTime = (int?)json["execution_time"];
+                raw = json;
+            }
+
+            public string? bannerUrl { get; set; }
+            public string? bannerImage { get; set; }
+            public string? language { get; set; }
+            public int? statusCode { get; set; }
+            public int? delayTime { get; set; }
+            public JObject raw { get; set; }
+        }
+
+        public class UserInfo
+        {
+            public UserInfo(JObject json)
+            {
+                ideas = (JArray?)json["data"]["ideas"];
+                challenges = (JArray?)json["data"]["challenges"];
+                colors = (JArray?)json["data"]["colors"];
+                isPrivate = (bool?)json["data"]["settings"]["private"];
+                statusCode = (int?)json["status_code"];
+                delayTime = (int?)json["execution_time"];
+                raw = json;
+            }
+
+            public JArray? ideas { get; set; }
+            public JArray? challenges { get; set; }
+            public JArray? colors { get; set; }
+            public bool? isPrivate { get; set; }
+            public int? statusCode { get; set; }
+            public int? delayTime { get; set; }
+            public JObject raw { get; set; }
+        }
+    }
+
     public class main
     {
         /// <summary>
@@ -15,27 +114,18 @@ namespace Artilities
         /// <para>Success: Dictionary containing keys: russian, english, statusCode, delayTime</para>
         /// <para>Error: Returns null</para>
         /// </returns>
-        public static Dictionary<string, string> GetIdea()
+        public static Responses.Idea? GetIdea()
         {
-            Dictionary<string, string> responseDictionary = new Dictionary<string, string>();
             dynamic response = JObject.Parse(Request.GET("https://artilities.herokuapp.com/api/ideas"));
             if (response == null)
             {
                 return null;
             }
 
-            var english = response.generated_idea.eng;
-            var russian = response.generated_idea.ru;
-            var statusCode = response.status_code;
-            var delayTime = response.execution_time;
-            if (statusCode == 200)
+            if (response["status_code"] == 200)
             {
-                responseDictionary.Add("russian", russian.ToString());
-                responseDictionary.Add("english", english.ToString());
-                responseDictionary.Add("statusCode", statusCode.ToString());
-                responseDictionary.Add("delayTime", delayTime.ToString());
-                responseDictionary.Add("raw", response.ToString());
-                return responseDictionary;
+                Responses.Idea r = new Responses.Idea(response);
+                return r;
             }
             return null;
         }
@@ -46,26 +136,18 @@ namespace Artilities
         /// <para>Success: Dictionary containing keys: russian, english, statusCode, delayTime</para>
         /// <para>Error: Returns null</para>
         /// </returns>
-        public static Dictionary<string, string> GetChallenge()
+        public static Responses.Challenge? GetChallenge()
         {
-            Dictionary<string, string> responseDictionary = new Dictionary<string, string>();
             dynamic response = JObject.Parse(Request.GET("https://artilities.herokuapp.com/api/challenges"));
             if (response == null)
             {
                 return null;
             }
-            var english = response.generated_challenge.eng;
-            var russian = response.generated_challenge.ru;
-            var statusCode = response.status_code;
-            var delayTime = response.execution_time;
-            if (statusCode == 200)
+
+            if (response["status_code"] == 200)
             {
-                responseDictionary.Add("english", english.ToString());
-                responseDictionary.Add("russian", russian.ToString());
-                responseDictionary.Add("statusCode", statusCode.ToString());
-                responseDictionary.Add("delayTime", delayTime.ToString());
-                responseDictionary.Add("raw", response.ToString());
-                return responseDictionary;
+                Responses.Challenge r = new Responses.Challenge(response);
+                return r;
             }
             return null;
         }
@@ -77,48 +159,24 @@ namespace Artilities
         /// <returns>
         /// <param>Success: Returns Dictionary containing keys: statusCode, delayTime, word, description</param>
         /// <param>Error: Returns null</param>
+        /// <parm>Will return 'word' and 'description' as null, if word isn't found</parm>
         /// </returns>
-        public static Dictionary<string, string> GetDictionaryEntry(string query)
+        public static Responses.DictionaryEntry? GetDictionaryEntry(string query)
         {
             int counter = 0;
-            Dictionary<string, string> responseDictionary = new Dictionary<string, string>();
             query = query.Replace(" ", "%20");
             dynamic response = JObject.Parse(Request.GET("https://artilities.herokuapp.com/api/dict?query=" + query));
             if (response == null)
             {
                 return null;
             }
-            var statusCode = response.status_code;
-            var delayTime = response.execution_time;
-            foreach (var item in response.query_results)
-            {
-                counter++;
 
-            }
-            if (counter > 0)
+            if (response["status_code"] == 200)
             {
-                var word = response.query_results[0][0];
-                var description = response.query_results[0][1];
-                if (statusCode == 200)
-                {
-                    responseDictionary.Add("statusCode", statusCode.ToString());
-                    responseDictionary.Add("delayTime", delayTime.ToString());
-                    responseDictionary.Add("word", word.ToString());
-                    responseDictionary.Add("description", description.ToString());
-                    responseDictionary.Add("raw", response.ToString());
-                    return responseDictionary;
-                }
-                return responseDictionary;
+                Responses.DictionaryEntry r = new Responses.DictionaryEntry(response);
+                return r;
             }
-            else
-            {
-                responseDictionary.Add("statusCode", statusCode.ToString());
-                responseDictionary.Add("delayTime", delayTime.ToString());
-                responseDictionary.Add("word", null);
-                responseDictionary.Add("description", null);
-                responseDictionary.Add("raw", response.ToString());
-                return responseDictionary;
-            }
+            return null;
         }
     }
 
@@ -131,53 +189,92 @@ namespace Artilities
         /// <para>Success: Dictionary containing keys: bannerUrl, bannerImage, language, statusCode, delayTime</para>
         /// <para>Error: Returns null</para>
         /// </returns>
-        public static Dictionary<string, string> GetBanners()
+        public static Responses.Banner? GetBanners()
         {
-            Dictionary<string, string> responseDictionary = new Dictionary<string, string>();
             dynamic response = JObject.Parse(Request.GET("https://artilities.herokuapp.com/api/other/banners"));
             if (response == null)
             {
                 return null;
             }
 
-            var bannerUrl = response.details.banner_url;
-            var bannerImage = response.details.banner_image;
-            var language = response.details.language;
-            var statusCode = response.status_code;
-            var delayTime = response.execution_time;
-            if (statusCode == 200)
+            if (response["status_code"] == 200)
             {
-                responseDictionary.Add("bannerUrl", bannerUrl.ToString());
-                responseDictionary.Add("bannerImage", bannerImage.ToString());
-                responseDictionary.Add("language", language.ToString());
-                responseDictionary.Add("statusCode", statusCode.ToString());
-                responseDictionary.Add("delayTime", delayTime.ToString());
-                responseDictionary.Add("raw", response.ToString());
-                return responseDictionary;
+                Responses.Banner r = new Responses.Banner(response);
+                return r;
             }
             return null;
+
         }
     }
-
-    internal class Request
+    public class users
     {
-        public static string GET(string URI)
+        public static string devkey;
+        public static string userID;
+
+        /// <summary>
+        /// Returns the saved challenges of a user
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns>
+        ///<para>Success: Dictionary containing keys: raw, challenges, statusCode, delayTime</para>
+        /// </returns>
+        public static Responses.UserInfo? GetUserInfo(string user = null)
         {
-            try
+            if (String.IsNullOrEmpty(user)) { user = userID; }
+            dynamic response = JObject.Parse(Request.UGET("https://artilities.herokuapp.com/api/user/getinfo", user));
+            if (response["status_code"] == 200)
             {
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URI);
-                request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
-                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
-                using (Stream stream = response.GetResponseStream())
-                using (StreamReader reader = new StreamReader(stream))
+                Responses.UserInfo r = new Responses.UserInfo(response);
+                return r;
+            }
+
+            return null;
+        }
+
+        internal class Request
+        {
+            public static string GET(string URI)
+            {
+                try
                 {
-                    return reader.ReadToEnd();
+                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URI);
+                    request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+                    using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                    using (Stream stream = response.GetResponseStream())
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        return reader.ReadToEnd();
+                    }
+                }
+                catch (Exception e)
+                {
+                    Trace.WriteLine("Server response failed");
+                    throw new Exception("Something went wrong during the request. Error: " + e);
                 }
             }
-            catch
+            public static string UGET(string URI, string searched_user)
             {
-                Trace.WriteLine("Server response failed");
-                return null;
+
+                try
+                {
+                    if (String.IsNullOrEmpty(users.userID)) { throw new Exception("Please Specify a userID for the DevKey!"); }
+                    if (String.IsNullOrEmpty(users.devkey)) { throw new Exception("Please specify a DevKey!"); }
+                    URI = URI + $"/?devkey={users.devkey}&userid={users.userID}&searched_userid={searched_user}";
+                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URI);
+                    request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+                    using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                    using (Stream stream = response.GetResponseStream())
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        return reader.ReadToEnd();
+                    }
+                }
+                catch (Exception e)
+                {
+                    Trace.WriteLine("Server response failed");
+                    throw new Exception("Something went wrong during the request. Error: " + e);
+                    return null;
+                }
             }
         }
     }
